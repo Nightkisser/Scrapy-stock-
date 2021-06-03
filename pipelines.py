702ed3b735d8687590d6353e8230eb87f16bed76
zc_night <66929977+Nightkisser@users.noreply.github.com>
2021-06-03 23:05:48 +0800

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -13,6 +13,7 @@ import settings
 
 class StockPipeline:
     def __init__(self):
+        # 连接MySQL数据库
         self.con = pymysql.connect(
             host=settings.MYSQL_HOST,  # host为数据库所在服务的ip地址
             port=settings.MYSQL_PORT,  # 端口号
@@ -21,20 +22,27 @@ class StockPipeline:
             db=settings.MYSQL_DB,  # 数据库名
             charset='utf8',  # 编码格式，一定要写这个，否则会出现乱码
         )
+        # 定位游标
         self.cursor = self.con.cursor()
 
     def process_item(self, item, spider):
+        # 定义sql语句，比如创建表、插入数据
         sql = "insert into stocks values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,curdate())"
+        # 执行sql语句，插入数据
         self.cursor.execute(sql,(item['stock_id'],item['stock_name'],item['cur_value'],item['change_value'],item['change_rate'],
                                  item['deal_num'],item['deal_money'],item['deal_rate'],item['trans_rate'],item['market_sale'],
                                  item['market_rate'],item['stock_loc']))
         self.con.commit()
         return item
-
+    
+    # 定义一个爬虫结束调用函数
     def close_spider(self,spider):
+        # 将爬取的数据从MySQL数据库中输出到文件中
         filename = str(datetime.date.today()) + '.txt'
         path = '/home/zcreset/Mysql学习/爬虫数据/Stock/' + filename
         sql = "select * from stocks into outfile %s"
         self.cursor.execute(sql,path)
+        # 关闭游标
         self.cursor.close()
+        # 关闭数据库连接
         self.con.close()
